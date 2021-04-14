015f8e0cc33f9dd92959df8b7774573fcad69f80
a-xai <akylaib007@gmail.com>
2021-04-13 21:34:53 -0700

    added animation for functions

--- app/src/main/java/com/example/simplecalculator1/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator1/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.simplecalculator1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.GridLayout;
+import android.widget.Button;
 import android.widget.TextView;
 
 import io.kaen.dagger.ExpressionParser;
@@ -17,49 +16,108 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView inputNumber;
     private TextView outputNumber;
-    private GridLayout gridLayout;
-    private ConstraintLayout constraintLayout;
+    private ConstraintLayout constraintLayoutFun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gridLayout = findViewById(R.id.grid_layout_for_numbers);
+
         setupUI();
-        openFragment();
+        openContainerFun();
     }
 
-    private void openFragment() {
-//        findViewById(R.id.fab).setOnClickListener(v -> {
-//            FragmentManager manager = getSupportFragmentManager();
-//            FragmentTransaction transaction = manager.beginTransaction().add(R.id.more_func,new OtherFunctionFragment());
-//            transaction.commit();
-//            gridLayout.setOnClickListener(v1 -> {
-//                FragmentTransaction transaction1 = manager.beginTransaction().hide(new OtherFunctionFragment());
-//                transaction1.commit();
-//            });
-//            constraintLayout.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Log.e("ololo", "clicked");
-//                    FragmentTransaction transaction1 = manager.beginTransaction().hide(new OtherFunctionFragment());
-//                    transaction1.commit();
-//                }
-//            });
-//        });
-    }
+    private void openContainerFun() {
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(constraintLayoutFun, "alpha", 0f, 1f);
+        fadeOut.setDuration(600);
+        ObjectAnimator fadeOutButton = ObjectAnimator.ofFloat((Button) findViewById(R.id.fab2), "alpha", 0f, 1f);
+        fadeOutButton.setDuration(600);
 
+        ObjectAnimator fadeOutButton2 = ObjectAnimator.ofFloat((Button) findViewById(R.id.fab), "alpha", 0f, 1f);
+        fadeOutButton2.setDuration(800);
+
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(constraintLayoutFun, "alpha", 1f, 0f);
+        ObjectAnimator fadeInButton = ObjectAnimator.ofFloat(findViewById(R.id.fab2), "alpha", 1f, 0f);
+        fadeInButton.setDuration(450);
+        fadeIn.setDuration(600);
+
+        fadeInButton.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                findViewById(R.id.fab2).setVisibility(View.GONE);
+                findViewById(R.id.fab).setVisibility(View.VISIBLE);
+                fadeOutButton2.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        fadeIn.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                constraintLayoutFun.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        findViewById(R.id.fab).setOnClickListener(v -> {
+            findViewById(R.id.fab).setVisibility(View.GONE);
+            fadeOut.start();
+            constraintLayoutFun.setVisibility(View.VISIBLE);
+
+            fadeOutButton.start();
+            findViewById(R.id.fab2).setVisibility(View.VISIBLE);
+        });
+
+        findViewById(R.id.fab2).setOnClickListener(v -> {
+            fadeIn.start();
+            fadeInButton.start();
+        });
+    }
 
     private void setupUI() {
+        constraintLayoutFun = findViewById(R.id.more_functions_container);
         inputNumber = findViewById(R.id.first_txt);
         outputNumber = findViewById(R.id.second_txt);
         ((TextView) findViewById(R.id.sign_delete_txt)).setOnClickListener((View.OnClickListener) v -> {
-            inputNumber.setText(inputNumber.getText().toString().substring(0, inputNumber.getText().toString().length() - 1));
+            if (!inputNumber.getText().toString().isEmpty()) {
+                inputNumber.setText(inputNumber.getText().toString().substring(0, inputNumber.getText().toString().length() - 1));
 
-            realTimeCalculate();
+                realTimeCalculate();
+            }
         });
-
-
+        ((TextView) findViewById(R.id.sign_delete_txt)).setOnLongClickListener(v -> {
+            inputNumber.setText("");
+            outputNumber.setText("");
+            return false;
+        });
     }
 
     public void onClickButton(View view) {
@@ -68,21 +126,30 @@ public class MainActivity extends AppCompatActivity {
 
         realTimeCalculate();
         getResultOfDecision();
+    }
 
+    public void onClickButtonTrigonometry(View view) {
+        String string = ((TextView) view).getText().toString();
+        inputNumber.setText(inputNumber.getText() + string + "(");
 
+        realTimeCalculate();
+        getResultOfDecision();
     }
 
     private void getResultOfDecision() {
         findViewById(R.id.sign_equal_txt).setOnClickListener(v -> {
             inputNumber.setText(outputNumber.getText());
             outputNumber.setText("");
-
+        });
+        findViewById(R.id.sign_equal_txt).setOnLongClickListener(v -> {
+            inputNumber.setText("");
+            return true;
         });
     }
 
     private void realTimeCalculate() {
         try {
-            outputNumber.setText(String.valueOf(new ExpressionParser().evaluate(inputNumber.getText().toString(), 3)));
+            outputNumber.setText(String.valueOf(new ExpressionParser().evaluate(inputNumber.getText().toString(), 9)));
         } catch (Exception e) {
             outputNumber.setText("0");
         }
